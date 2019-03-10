be43443155762bba515d1e9f132b2119024e6db0
dogezhou <doge.zhou@outlook.com>
2019-03-10 16:57:28 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 !function(){
+    var duration = 20
     function writeCss(prefix, code, callback) {
         var container = document.querySelector('#code')
         var styleTag = document.querySelector('#styleTag')
         var n = 0
-        var timer = setInterval(() => {
+        // var timer = setInterval(() => {
+        //     n += 1
+        //     container.innerHTML = code.substring(0, n)
+        //     styleTag.innerHTML = code.substring(0, n)
+        //     container.scrollTop = container.scrollHeight
+        //     if (n >= code.length) {
+        //         window.clearInterval(timer)
+        //         callback && callback()
+        //     }
+        // }, 10);
+        setTimeout(function fn() {
             n += 1
             container.innerHTML = code.substring(0, n)
             styleTag.innerHTML = code.substring(0, n)
             container.scrollTop = container.scrollHeight
-            if (n >= code.length) {
-                window.clearInterval(timer)
+            if (n < code.length) {
+                setTimeout(fn, duration)
+            } else {
                 callback && callback()
             }
-        }, 10);
+        }, duration);
     }
+
     var css = `
 /*
  * 我叫 dogezhou
@@ -181,4 +194,23 @@
 /* 一只皮卡丘！谢谢观看！*/
     `
     writeCss('', css)
+
+    // 调速
+    $('.actions').on('click', 'button', function(e) {
+        var $button = $(e.currentTarget)
+        var speed = $button.attr('data-speed')
+        $button.addClass('active')
+            .siblings('.active').removeClass('active')
+        switch(speed) {
+            case 'slow':
+                duration = 100
+                break
+            case 'normal':
+                duration = 50
+                break
+            case 'fast':
+                duration = 10
+                break
+        }
+    })
 }();
